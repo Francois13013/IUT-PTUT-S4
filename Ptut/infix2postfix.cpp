37ad7b314ede02cafe7bf35525d367b9d62a0ca0
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2020-03-06 10:08:20 +0100

    traduction de infix2postfix pour notre utilisation en C++

--- Ptut/infix2postfix.cpp
+++ Ptut/infix2postfix.cpp
@@ -1,13 +1,16 @@
 // -----------------------------------------------------------------------------
 // Transformation d'une formule logique infixe en forme postfixe
-// 10/02/2020, V. Risch
+// 10/02/2020, V. Risch, J. Pouzargues
+//
+// Le code initial a été conçu pour du C, l'utilisant en C++, il a été traduit
+// et les piles ont été remplacés par des string afin de faciliter l'utilisation pour notre démonstrateur.
 //
 // Le programme recupere une formule logique en forme infixe donnee sous la
-// forme d'un tableau de caracteres f_infixe pour la transmettre a la fonction
+// forme d'une string formuleInfixe pour la transmettre a la fonction
 // Infix2prefix qui la transforme en forme postfixe. Infix2Prefix gere deux
-// piles : une pile des operateurs pile_operateurs et f_postfixe vue
-// initialement comme pile des operandes a laquelle les operateurs sont ajoutes
-// au fur et a mesure qu'ils sont depiles de pile_operateurs. Les operandes sont
+// string : une string des operateurs et formulePostfixe vue
+// initialement comme string des operandes a laquelle les operateurs sont ajoutes
+// au fur et a mesure qu'ils sont enlevés de operateurs. Les operandes sont
 // tout caractere minuscule entre 'a' et 'z', les operateurs sont
 // '#': modalite []
 // '%': modalite <>
@@ -23,50 +26,6 @@
 #include <iostream>
 using namespace std;
 // #include <stdlib.h>
-
-#define F_SIZEMAX 51        // taille max d'une formule incluant '\n' comme
-                            // caractere de fin de chaine
-
-// -----------------------------------------------------------------------------
-// Structures de donnees
-// -----------------------------------------------------------------------------
-
-typedef struct pile_symboles {
-  unsigned int top;
-  char symboles[F_SIZEMAX];
-} PILE_SYMBOLES;            // pile de caracteres
-
-PILE_SYMBOLES * f_postfixe;
-
-char f_infixe[F_SIZEMAX];   // formule infixe sous forme d'un tableau de
-                            // caracteres
-unsigned int taille_infixe;
-
-// -----------------------------------------------------------------------------
-// Gestion de la pile
-// -----------------------------------------------------------------------------
-
-char NextChar (void) {
-  // Renvoie le prochain caractere trouve sur l'entree standart
-  // a l'exception des blancs
-
-  char c;
-
-  while ((c=getchar()) == ' ') {}
-  return c;
-}
-
-void PushChar(char c, PILE_SYMBOLES * pile) {
-  // Empile le caractere lu
-
-  pile->symboles[pile->top++]=c;
-}
-
-char PopChar(PILE_SYMBOLES * pile) {
-   // Depile le caractere lu */
-
-  return pile->symboles[--pile->top];
-}
 
 // -----------------------------------------------------------------------------
 // Transformation Infix2prefix
@@ -114,91 +73,91 @@ int IsProposition(char c) {
   return ((c>='a')&&(c<='z'));
 }
 
-PILE_SYMBOLES * Infix2Postfix (unsigned int taille_infixe, char * f_infixe) {
-  // Transforme la formule infixe en expression postfixe, terminee par '\n'
-  // La formule infixe initiale est lue de droite à gauche afin de preserver
-  // l'associativité à gauche implicitement associée à l'absence de parentheses
-  // La pile initiale des opérandes sert de pile de renvoi de la formule
-  // sous forme postfixe et, comme variable locale, est donc declaree static
+string infix2Postfix (string formuleInfixe) {
+    string operandes;
+    string operateurs;
 
-  static
-  PILE_SYMBOLES tableau_operandes={0};               // sert a la fois de pile des operandes et
-  PILE_SYMBOLES * pile_operandes=&tableau_operandes; // de pile de renvoi de la formule postfixe
+    char c;
 
-  PILE_SYMBOLES tableau_operateurs={0};
-  PILE_SYMBOLES * pile_operateurs=&tableau_operateurs;
-
-  signed int i;
-  char c;
-
-  for (i=taille_infixe-1;(i>=0);--i) {
-    c=f_infixe[i];
-    if (IsOperator(c)) {
-      while ((pile_operateurs->symboles[pile_operateurs->top-1] != ')') && (Precedence(pile_operateurs->symboles[pile_operateurs->top-1])>=Precedence(c)))
-        PushChar(PopChar(pile_operateurs), pile_operandes);
-      PushChar(c, pile_operateurs);
+    for (int i = formuleInfixe.size() - 1; i >= 0; --i)
+    {
+        c = formuleInfixe[i];
+        if (IsOperator(c))
+        {
+            while ((operateurs[operateurs.size() - 1] != ')') && (Precedence(operateurs[operateurs.size() - 1])>=Precedence(c)))
+            {
+                operandes.push_back(operateurs[operateurs.size()-1]);
+                operateurs.pop_back();
+            }
+            operateurs.push_back(c);
+        }
+        else
+        {
+            if (IsProposition(c)) operandes.push_back(c);
+            else // c'est une parenthèse
+                if (c == ')') operateurs.push_back(c);
+                else
+                {
+                    if (c == '(') {
+                        while (operateurs[operateurs.size() - 1] != ')')
+                        {
+                            operandes.push_back(operateurs[operateurs.size()-1]);
+                            operateurs.pop_back();
+                        }
+                        operateurs.pop_back();
+                    }
+                }
+        }
     }
-    else
-      if (IsProposition(c)) PushChar(c, pile_operandes);
-      else // c est une parenthese
-        if (c==')') PushChar(c, pile_operateurs);
-    else
-          if (c=='(') {
-            while (pile_operateurs->symboles[pile_operateurs->top-1] != ')')
-              PushChar(PopChar(pile_operateurs), pile_operandes);
-        pile_operateurs->top--;
-      }
-  }
-  PushChar('\n', pile_operandes);
-
-  return pile_operandes;
+    operandes.push_back('\n');
+    return operandes;
 }
 
 // -----------------------------------------------------------------------------
-// Entree-sortie
+// Entree
 // -----------------------------------------------------------------------------
 
-unsigned int Lire (char* formule) {
-  // Lit une formule sous forme d'un tableau de caracteres, encadre par defaut
-  // d'une parenthese gauche et d'une parenthese droite et se finissant par '\n'
-  // Le parenthesage explicite de la formule assure la coherence des tests
-  // ulterieurs et est conforme à la definition syntaxique de la notion de
-  // formule
+string Read () {
+    string formule;
+    getline(cin, formule);
 
-  unsigned int longueur=0;
+    formule.insert(0,"(");
+    formule.push_back(')');
+    formule.push_back('\n');
 
-  formule[F_SIZEMAX] = '\n';   // sentinelle
-  formule[longueur]='(';
-  while ((formule[++longueur]=NextChar()) != '\n') {}
-  formule[longueur] = ')';
-  formule[++longueur] = '\n';
+    return formule;
 
-  return longueur;
 }
 
-void Afficher (char* formule) {
-  // Affiche une formule donnee sous forme d'un tableau de caracteres
 
-  unsigned int i;
+// fonction désormais inutile
 
-  for(i=0;(formule[i] != '\n');++i) printf("%c",formule[i]);
-  printf("\n");
-}
+//string transformToString (char* formule) {
+//    // transforme une formule donnee en une chaîne de caractère
 
+//    string stringFormule;
+
+//    unsigned int i;
+
+//    for(i=0;(formule[i] != '\n');++i) stringFormule.push_back(formule[i]);
+
+//    cout << stringFormule << '\n';
+
+//    return stringFormule;
+//}
 
 // -----------------------------------------------------------------------------
 // main
 // -----------------------------------------------------------------------------
 
-//int main (void) {
-//  taille_infixe = Lire(f_infixe);
+int main (void) {
 
-//  printf("taille_infixe = %d\n", taille_infixe);
-//  Afficher(f_infixe);
 
-//  f_postfixe=Infix2Postfix(taille_infixe, f_infixe);
+    string formuleInfixe = Read();
+    cout << "Taille infixe = " << formuleInfixe.size() - 3 << endl;
+    string formulePostfix = infix2Postfix(formuleInfixe);
+    cout << formulePostfix;
 
-//  Afficher(f_postfixe->symboles);
 
-//  return 0;
-//}
+  return 0;
+}
